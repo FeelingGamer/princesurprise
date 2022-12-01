d22a7c0e297058e10cafbfd240812be2b5d6b90e
MarkRanielPerez <20-06887@g.batstate-u.edu.ph>
2022-12-01 14:01:49 +0800

    firebase deployment

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'princesurprise'`, () => {
+  it(`should have as title 'finalprincebee'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('princesurprise');
+    expect(app.title).toEqual('finalprincebee');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('princesurprise app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('finalprincebee app is running!');
   });
 });
